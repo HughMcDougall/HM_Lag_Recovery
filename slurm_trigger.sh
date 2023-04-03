8a0a9d805a74bad004fc30edb1574e72df42ece9
hughm <hughmcdougallemail@gmail.com>
2023-04-04 01:17:47 +1000

    auto commit via batch

--- slurm_trigger.sh
+++ slurm_trigger.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
-#SBATCH --job-name=getafixtestjob
+#SBATCH --job-name=LineFit01
 #SBATCH --nodes=1
-#SBATCH --ntasks-per-node=6
-#SBATCH --mem-per-cpu=32G
-#SBATCH --time=0-10:00:00
+#SBATCH --ntasks-per-node=1
+#SBATCH --mem-per-cpu=0.5G
+#SBATCH --time=0-00:10:00
+#SBATCH --array=0-0
+#SBATCH --mail-type=BEGIN,END,FAIL
+#SBATCH --mail-user=hmgetafix@gmail.com
 
 # Load the necessary modules
 module load anaconda3/5.2.0
@@ -13,7 +16,7 @@ conda activate lag_conda
 python activate_test.py > _start.txt
 
 # Run your Python script with SLURM_ARRAY_TASK_ID as argument
-python LineFit01.py -Ncores 6
+python LineFit01.py -Ncores 1 -Nchains 1 -Nburn 0 -Nsamples 1 -i $SLURM_ARRAY_TASK_ID
 
 #If finished properly, print the time to a 'done' file
 python activate_test.py > _done.txt
