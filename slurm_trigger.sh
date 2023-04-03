fbfc56461f9d9c4add2923c8ea76ec14ed770dbe
hughm <hughmcdougallemail@gmail.com>
2023-04-04 01:18:24 +1000

    auto commit via batch

--- slurm_trigger.sh
+++ slurm_trigger.sh
@@ -2,7 +2,7 @@
 #SBATCH --job-name=LineFit01
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
-#SBATCH --mem-per-cpu=0.5G
+#SBATCH --mem-per-cpu=500M
 #SBATCH --time=0-00:10:00
 #SBATCH --array=0-0
 #SBATCH --mail-type=BEGIN,END,FAIL
