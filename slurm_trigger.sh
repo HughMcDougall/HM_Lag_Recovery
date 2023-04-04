05121294fdbdbc7ff3d254642f539c0bc9e4b578
Hugh McDougall <uqhmcdou@fac-login-1.local>
2023-04-05 02:28:10 +1000

    SA long

--- slurm_trigger.sh
+++ slurm_trigger.sh
@@ -2,7 +2,7 @@
 #SBATCH --job-name=HM_line_fit-job-%j
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
-#SBATCH --cpus-per-task 1
+#SBATCH --cpus-per-task 8
 #SBATCH --mem-per-cpu=4G
 #SBATCH --time=0-10:00:00
 #SBATCH --array=0-0
@@ -16,4 +16,4 @@ eval "$(conda shell.bash hook)"
 conda activate lag_conda
 
 # Run your Python script with SLURM_ARRAY_TASK_ID as argument
-python LineFit01.py -Ncores 1 -Nchains 1 -Nburn 200 -Nsamples 600 -i $SLURM_ARRAY_TASK_ID -progress_bar 1 -table SIMBAtest_status.dat
+python LineFit01.py -Ncores 8 -Nchains 8 -Nburn 2000 -Nsamples 6000 -i $SLURM_ARRAY_TASK_ID -progress_bar 1 -table SIMBAtest_status.dat
