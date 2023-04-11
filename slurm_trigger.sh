6ffc62e95ca9965f7a569a9ce43e0c2314794e07
hughm <hughmcdougallemail@gmail.com>
2023-04-11 23:07:15 +1000

    auto commit via batch

--- slurm_trigger.sh
+++ slurm_trigger.sh
@@ -4,7 +4,7 @@
 #SBATCH --ntasks-per-node=1
 #SBATCH --cpus-per-task 6
 #SBATCH --mem-per-cpu=4G
-#SBATCH --time=0-5:00:00
+#SBATCH --time=0-10:00:00
 #SBATCH --array=0-5
 #SBATCH --mail-type=BEGIN,END,FAIL
 #SBATCH --mail-user=hmgetafix@gmail.com
@@ -16,4 +16,4 @@ eval "$(conda shell.bash hook)"
 conda activate lag_conda
 
 # Run your Python script with SLURM_ARRAY_TASK_ID as argument
-python LineFit01.py -Ncores 8 -Nchain 120 -Nburn 200 -Nsamples 200 -i $SLURM_ARRAY_TASK_ID -progress_bar 1 -table SIMBAtest_status.dat
+python LineFit01.py -Ncores 8 -Nchain 300 -Nburn 200 -Nsamples 200 -i $SLURM_ARRAY_TASK_ID -progress_bar 1 -table SIMBAtest_status.dat
