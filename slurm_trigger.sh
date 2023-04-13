69f8e17b512bd83a3387f9f4868dbb703a8e9ad6
hughm <hughmcdougallemail@gmail.com>
2023-04-13 16:49:00 +1000

    auto commit via batch

--- slurm_trigger.sh
+++ slurm_trigger.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 #SBATCH --job-name=HM_line_fit-job-%j
-#SBATCH --nodes=6
 #SBATCH --ntasks-per-node=1
-#SBATCH --cpus-per-task 24
-#SBATCH --mem-per-cpu=4G
-#SBATCH --time=0-10:00:00
-#SBATCH --array=0-5
+#SBATCH --cpus-per-task 1
+#SBATCH --mem-per-cpu=8G
+#SBATCH --time=2-00:00:00
+#SBATCH --array=0-92
 #SBATCH --mail-type=BEGIN,END,FAIL
 #SBATCH --mail-user=hmgetafix@gmail.com
 #SBATCH --output=./slurm_logs/slurm-%j.out
@@ -16,4 +15,4 @@ eval "$(conda shell.bash hook)"
 conda activate lag_conda
 
 # Run your Python script with SLURM_ARRAY_TASK_ID as argument
-python LineFit01.py -Ncores 24 -Nchain 24 -Nburn 200 -Nsamples 200 -i $SLURM_ARRAY_TASK_ID -progress_bar 1 -table SIMBAtest_status.dat
+python LineFit01.py -Ncores 1 -Nchain 300 -Nburn 200 -Nsamples 200 -i $SLURM_ARRAY_TASK_ID -progress_bar 1
